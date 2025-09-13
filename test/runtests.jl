d8ce83cb25800501829005e03acb00069adb7a86
Cuber <cuber.ycyin@gmail.com>
2025-09-14 00:21:32 +0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,6 @@
 using Test
 using KerrGeodesics
 
-@testset "basic" begin
-    # a tiny smoke test;
-    vals = Kerr_Geodesics(0.0, 10.0, 0.5, 0.8)
-    @test isa(vals, Dict) || isa(vals, NamedTuple)
+@testset "KerrGeodesics.jl" begin
+    # Nothing here for now
 end
